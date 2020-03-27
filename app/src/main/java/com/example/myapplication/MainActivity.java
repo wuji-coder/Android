028a23829794fbf1b959f07d8ed533171495eb36
无极 <1927576488@qq.com>
2020-03-27 19:39:05 +0800

    布局

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_linear_layout);
+        setContentView(R.layout.activity_table_layout);
+        setContentView(R.layout.activity_relative_layout);
     }
 }
